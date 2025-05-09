8c94b6ec7c3c8f005424e1495f0b0f80c59e29af
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2025-05-09 18:14:03 -0500

    style: update BookingPill component for improved layout and spacing, enhancing visual consistency

--- apps/web/src/components/common/booking-pill.tsx
+++ apps/web/src/components/common/booking-pill.tsx
@@ -5,36 +5,38 @@ import { Separator } from '@/components/ui/separator';
 export function BookingPill() {
   return (
     <div
-      className="flex gap-10 justify-start items-center px-9 py-3 rounded-full bg-background border border-border w-fit"
+      className="flex gap-14 justify-start items-center px-10 py-4 rounded-full bg-background border border-border w-fit"
     >
-      <div className="flex flex-col items-start justify-start hover:cursor-pointer group">
+      <div
+        className="flex flex-col items-start justify-start hover:cursor-pointer group"
+      >
         <p
-          className="text-xs text-muted-foreground group-hover:text-primary transition-all duration-200"
+          className="text-sm text-muted-foreground group-hover:text-primary transition-all duration-200"
         >
           Check In
         </p>
-        <p className="text-sm font-medium">Add Date</p>
+        <p className="font-medium">Add Date</p>
       </div>
-      <Separator orientation="vertical" className="bg-border h-6!" />
+      <Separator orientation="vertical" className="bg-border h-8!" />
       <div className="flex flex-col items-start justify-start hover:cursor-pointer group">
         <p
-          className="text-xs text-muted-foreground group-hover:text-primary transition-all duration-200"
+          className="text-sm text-muted-foreground group-hover:text-primary transition-all duration-200"
         >
           Check Out
         </p>
-        <p className="text-sm font-medium">Add Date</p>
+        <p className="font-medium">Add Date</p>
       </div>
-      <Separator orientation="vertical" className="bg-border h-6!" />
+      <Separator orientation="vertical" className="bg-border h-8!" />
       <div className="flex flex-col items-start justify-start hover:cursor-pointer group">
         <p
-          className="text-xs text-muted-foreground group-hover:text-primary transition-all duration-200"
+          className="text-muted-foreground group-hover:text-primary transition-all duration-200"
         >
           Guests
         </p>
-        <p className="text-sm font-medium">Add Guests</p>
+        <p className="font-medium">Add Guests</p>
       </div>
       <SearchIcon
-        className="w-5 h-5 hover:cursor-pointer text-muted-foreground hover:text-primary transition-all duration-200"
+        className="w-7 h-7 hover:cursor-pointer text-muted-foreground hover:text-primary transition-all duration-200"
       />
     </div>
   );
