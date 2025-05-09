c3a417468142962cfceed68954ce8e34e74e5134
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2025-05-09 14:41:05 -0500

    feat: add initial seed data for rooms in Supabase

---
+++ packages/db/supabase/seed.sql
@@ -0,0 +1,6 @@
+insert into rooms 
+  (name, description, price) 
+values
+  ('Queen Room', 'A room with a queen-sized bed', 100),
+  ('King Room', 'A room with a king-sized bed', 200),
+  ('Suite', 'A room with a king-sized bed and a separate living area', 300);
